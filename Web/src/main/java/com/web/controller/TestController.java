29e7ef57a663c7c7d0d3736e214552da79600c4b
tom_global <tom_global@review.com>
2020-12-31 20:32:37 +0800

    web 进行时

---
+++ Web/src/main/java/com/web/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author lijichen
+ * @date 2020/12/31 - 18:35
+ */
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/testController")
+    public Object testController() {
+        return new Date();
+    }
+
+    @RequestMapping("/testThymeleaf")
+    public String testThymeleaf(Map<String, Object> map) {
+        map.put("thymeleaf","thymeleaf.html，经过thymeleaf模板解析");
+        map.put("users", Arrays.asList("qqq","www","<h1>eee</h1>"));
+        return "thymeleaf";
+    }
+}
